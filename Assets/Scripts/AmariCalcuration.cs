c4729ea4ad2d86890fd5b88ea2b64bc9be53ecc7
taisei <tatata0389@gmail.com>
2018-12-19 19:27:17 +0900

    Add Calcuration Method

---
+++ Assets/Scripts/AmariCalcuration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmariCalcuration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        
+    }
+
+    public int Amari(int a, int b)
+    {
+        return a % b;
+    }
+}
