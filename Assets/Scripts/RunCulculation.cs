68e52116d537d8e1a287f15f31af76cf05d43717
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2018-12-21 20:33:18 +0900

    Add RunCalculation.cs

---
+++ Assets/Scripts/RunCulculation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunCulculation : MonoBehaviour {
+
+    public int a;
+    public int b;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(GetComponent<AmariCalcuration>().Amari(a, b));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
